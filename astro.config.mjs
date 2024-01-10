51c6b3f11fb774fbcea359430d5b0183019bce99
Lava <34743145+CanglongCl@users.noreply.github.com>
2024-01-10 01:26:14 -0800

    image optimize

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,15 @@ import tailwind from "@astrojs/tailwind";
 import { remarkReadingTime } from "./src/remark-plugins/remark-reading-time.js";
 import { flowbiteImage } from "./src/remark-plugins/flowbite-image.js";
 import { popupSyntax } from "./src/remark-plugins/popup-syntax.js";
+import vercel from "@astrojs/vercel/static";
+import remarkEleventyImage from "astro-remark-eleventy-image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [
+    tailwind(),
+    remarkEleventyImage()
+  ],
   markdown: {
     remarkPlugins: [
       [remarkToc, { heading: "目录" }],
@@ -15,5 +20,15 @@ export default defineConfig({
       flowbiteImage,
       popupSyntax,
     ],
+  },
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
+  vite: {
+    ssr: {
+      external: ["@11ty/eleventy-img"],
+    },
   },
 });
